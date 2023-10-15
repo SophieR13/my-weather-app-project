05e959f362ba7f902f0d1b88ec2c1a9333a9bc9f
SophieR13 <sophiern13@gmail.com>
2023-10-15 17:11:39 +0100

    Showing current temp, humidity, speed of wind and weather description

--- src/index.js
+++ src/index.js
@@ -1,60 +1,18 @@
-let now = new Date();
-
-let h5 = document.querySelector("h5");
-
-let day = now.getDay();
-let hour = now.getHours();
-let minute = now.getMinutes();
-
-function formatDate(date) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let currentDay = days[day];
-  let currentHour = hour;
-  let currentMinute = minute;
-
-  h5.innerHTML = `${currentDay}, ${currentHour}:${currentMinute}`;
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
-formatDate(now);
+let apiKey = "d9accof3b38dc4420097tf7819c5b0b6";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
 
-//Second challenge
-
-const form = document.querySelector("#search-text-input");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the default form submission behavior
-
-  const input = form.querySelector("input[type='text']");
-  const value = input.value;
-
-  const cardTextCity = document.querySelector(".card-text-city");
-  cardTextCity.textContent = value;
-
-  let units = "metric";
-  let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
-
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}&units=${units}`;
-
-  axios.get(apiUrl).then(showTemperature);
-});
-
-//search engine
-
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  console.log(temperature);
-  console.log(response);
-  let city = response.data.name;
-  let message = `${temperature}° in`;
-  let h5 = document.querySelector("h5");
-  h5.innerHTML = message;
-}
+axios.get(apiUrl).then(displayTemperature);
