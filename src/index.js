96c95270d58d81332e3b115158a2e54fec5e27e4
SophieR13 <sophiern13@gmail.com>
2023-10-26 21:04:55 +0100

    Adjusted forecast

--- src/index.js
+++ src/index.js
@@ -32,18 +32,17 @@ function formatDay(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-  console.log(response.data.daily);
 
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 4) {
       forecastHTML =
         forecastHTML +
         `
-      <div class="col-2">
+      <div class="col">
         <div class="weather-forecast">
           <div class="row">
             <div class="card" style="width: 8rem">
